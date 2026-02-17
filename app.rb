9c0edeac0b23d1b11aaa5befcbfc01f466b72089
BrunoF <brerreira8@gmail.com>
2026-02-17 07:32:02 -0300

    Primeiro commit do desafio 1 de ruby.

---
+++ app.rb
@@ -0,0 +1,17 @@
+# ENTRADA DE DADOS 
+ print "Bem vindo ao cadastro de usuário da Hash Marketing Visual"
+
+  # RECEBE O NOME DO USUÁRIO
+ print "Por Favor Digite seu nome: "
+ nome = gets.chomp
+
+ # RECEBE A IDADE DO USUÁRIO
+ print "Digite a sua idade: "
+ idade = gets.chomp
+
+ # RECEBE A PROFISSÃO DO USUÁRIO
+ print "Digite a sua profissão: "
+ profissao = gets.chomp
+
+ print "Seja bem vindo: #{nome}, sua idade é: #{idade}, vou te transferir para um dos nossos atendentes. 😁 "
+
